8d43c852f883fcbc1552d5709237379d2b5260f3
Mahmudul <mahmudul@blubnc.com>
2024-11-27 15:06:40 +0600

    story

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import About from './components/About'
 import Features from './components/Features'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Story from './components/Story'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
 <Hero/>
 <About/>
 <Features/>
+<Story/>
 </main>
   )
 }
